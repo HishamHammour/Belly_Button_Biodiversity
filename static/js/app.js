e4fc62fab95e335588f4ab2e6b03933da3fc3582
HishamHammour <hishamhammour@gmail.comm>
2019-05-10 11:11:02 -0500

    updating requirements

--- static/js/app.js
+++ static/js/app.js
@@ -5,16 +5,16 @@ function buildMetadata(sample) {
   // Use `d3.json` to fetch the metadata for a sample
   // Use d3 to select the panel with id of `#sample-metadata`
 
-  // Use `.html("") to clear any existing metadata
-
-  // Use `Object.entries` to add each key and value pair to the panel
   // Hint: Inside the loop, you will need to use d3 to append new
   // tags for each key-value in the metadata.
 
   var url = `/metadata/${sample}`;
   d3.json(url).then(function(sample){
     var sample_metadata = d3.select("#sample-metadata");
+  // Use `.html("") to clear any existing metadata
     sample_metadata.html("");
+
+  // Use `Object.entries` to add each key and value pair to the panel
 
     Object.entries(sample).forEach(([key, value]) => {
       var row = sample_metadata.append("p");
